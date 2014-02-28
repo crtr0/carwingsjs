0acf292f1a071e25ca876dcb14ed764f902fe747
Carter Rabasa <carter.rabasa@gmail.com>
2014-02-28 15:35:24 -0800

    0.0.1 of the module

---
+++ README.md
@@ -0,0 +1,32 @@
+#CarwingsJS
+
+CarwingsJS is a Node module that provides bindings for the Nissan Leaf [Carwings][1] API. To be clear, this API is not officially documented externally and it is unclear how long Nissan will continue to allow 3rd party apps to connect to it. 
+
+## Installation
+
+`npm install carwingsjs`
+
+## Usage
+
+In your application 
+
+```
+var carwings = require('carwings');
+
+carwings.login({
+  leaf_username: 'username',
+  leaf_password: 'password',
+  device_token: 'device token',
+  device_id: 'unique device id'	
+}, function(err, output) {
+	console.log(output);
+})
+
+## Appendix
+
+1. [Nissan Leaf Forums - Carwings protocol][2]
+2. [Nissan Leaf Forums - Carwings API thread][3]
+
+[1]:http://www.nissanusa.com/innovations/carwings.article.html
+[2]:http://www.mynissanleaf.com/wiki/index.php?title=Carwings_protocol
+[3]:http://www.mynissanleaf.com/viewtopic.php?f=27&t=2214
--- index.js
+++ index.js
@@ -1,30 +1,42 @@
 var request  = require('request')
-  , xml2js   = require('xml2js')
+  , xpath    = require('xpath')
+  , dom      = require('xmldom').DOMParser
   , fs       = require('fs')
   , mustache = require('mustache')
-  , util     = require('util')
-  , config   = require('./config');
+  , util     = require('util');
 
-fs.readFile('payload.xml', 'utf-8', function (err, data) {
-  if (err) {
-    throw err;
-  }
+exports.login = function(config, callback) {
 
-  var output = mustache.render(data, config);
+  fs.readFile('payload.xml', 'utf-8', function (err, data) {
+    if (err) {
+      throw err;
+    }
 
-  request.post({
-    url: 'https://nissan-na-smartphone-biz.viaaq.com/aqPortal/smartphoneProxy/userService',
-    headers: {
-      'User-Agent': 'NissanLEAF/1.40 CFNetwork/485.13.9 Darwin/11.0.0 leafjs',
-      'Content-Type': 'text/xml'
+    var output = mustache.render(data, config);
+
+    request.post({
+      url: 'https://nissan-na-smartphone-biz.viaaq.com/aqPortal/smartphoneProxy/userService',
+      headers: {
+        'User-Agent': 'NissanLEAF/1.40 CFNetwork/485.13.9 Darwin/11.0.0 leafjs',
+        'Content-Type': 'text/xml'
+        },
+      jar: true,
+      body: output
       },
-    body: output
-    },
-    function(error, response, body) {
-      console.log(error);
-      obj = xml2js.parseString(body, function (err, result) {
-        console.log(util.inspect(result, false, null))
-      });
-    });
+      function(error, response, body) {
+        
+        var doc = new dom().parseFromString(body)
+          , select = xpath.useNamespaces({"ns3": "urn:com:hitachi:gdc:type:report:v1"})
+          , loginResponse = {};
 
-});
+        loginResponse._rawXML = body;        
+        loginResponse.batteryCapacity = select('//ns3:BatteryCapacity/text()', doc)[0].nodeValue;
+        loginResponse.batteryChargingStatus = select('//ns3:BatteryChargingStatus/text()', doc)[0].nodeValue;
+        loginResponse.pluginState = select('//ns3:PluginState/text()', doc)[0].nodeValue;
+        loginResponse.timeRequiredToFull = select('//ns3:HourRequiredToFull/text()', doc)[0].nodeValue + ":" + select('//ns3:MinutesRequiredToFull/text()', doc)[0].nodeValue;
+
+        callback(err, loginResponse);
+      });
+  });
+};
+
